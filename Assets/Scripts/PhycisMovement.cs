3ff00e757d34fc27ecfecf538b3201b75d4f8db6
Mike <mike02.oudshoorn@gmail.com>
2023-09-19 17:32:37 +0200

    added dynamic jumping

--- Assets/Scripts/PhycisMovement.cs
+++ Assets/Scripts/PhycisMovement.cs
@@ -8,8 +8,13 @@ public class PhycisMovement : MonoBehaviour
 {
     private float distToGround;
     private Rigidbody _rigidbody;
-
+    private bool spaceispressed;
+    [SerializeField] private float jumpTime = 0.35f;
+    [SerializeField] private float jumpTimeCounter;
     [SerializeField] private float force = 10;
+
+    private bool isJumping;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -19,20 +24,41 @@ public class PhycisMovement : MonoBehaviour
 
     bool IsGrounded()
     {
-         return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+         return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.2f);
     }
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Jump") && IsGrounded()) spaceispressed = true;
     }
     
 
     private void FixedUpdate()
     {
-        if (Input.GetKey("space") && IsGrounded())
+        if (spaceispressed)
         {
+            spaceispressed = false;
+            isJumping = true;
+            jumpTimeCounter = jumpTime;
             _rigidbody.AddForce(Vector3.up * force, ForceMode.Impulse);
+        }
+        if(Input.GetButton("Jump") && isJumping)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                _rigidbody.AddForce(Vector3.up * 0.5f, ForceMode.Impulse);
+                jumpTimeCounter -= Time.deltaTime;
+                
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            isJumping = false;
         }
     }
 }
