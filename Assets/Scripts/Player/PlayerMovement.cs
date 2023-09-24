e45e54b03ff20bb28a248a11511e1707366be04e
tusmavan <17132304@student.hhs.nl>
2023-09-24 21:38:08 +0200

    Merge with dev

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,25 +1,28 @@
-
-using System;
 using System.Collections;
-
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Components")] 
+    [SerializeField] private TrailRenderer tr;
+    private Rigidbody _rigidbody;
+
     //sets the speed of the player.
-    [SerializeField] private float speed = 6f;
-    [SerializeField] private float turnSmoothTime = 0.1f;
-    [SerializeField] private float dashingPower = 12f;
+    [Header("Movement")] [SerializeField] private float speed = 6f;
+
+    [Header("Turning")] [SerializeField] private float turnSmoothTime = 0.1f;
+
+    [Header("Dashing")] [SerializeField] private float dashingPower = 12f;
+
     [SerializeField] private float dashingTime = 0.5f;
     [SerializeField] private float dashingCooldown = 1f;
-    [SerializeField] private TrailRenderer tr;
     private Camera _camera;
     private bool _canDash = true;
     private bool _isDashing;
     private Vector3 _lastDirection;
-
-    private Rigidbody _rigidbody;
-
+    private Vector2 _movement = Vector2.zero;
+    
     private float _turnSmoothVelocity;
 
     private void Start()
@@ -28,29 +31,42 @@ public class PlayerMovement : MonoBehaviour
         _rigidbody = GetComponent<Rigidbody>();
     }
 
-
-    
-
     private void Update()
     {
         if (_isDashing) return;
-        CheckDash();
-        CheckSprint();
-        PlayerMove(); 
+        PlayerMove();
     }
 
-    Vector3 PlayerInput()
+    private void OnMove(InputValue inputValue)
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical);
-        return direction;
+        _movement = inputValue.Get<Vector2>();
     }
 
-    void PlayerMove()
+    private void OnSprintStart()
+    {
+        speed = 12f;
+    }
+
+    private void OnSprintFinish()
+    {
+        speed = 6f;
+    }
+
+    private void OnDash()
+    {
+        if (_canDash) StartCoroutine(Dash());
+    }
+
+    private void PlayerMove()
     {
         //moves the player. taking into account the delta time, world space, and the speed.
-            transform.Translate(GetDirection(PlayerInput()).normalized * (speed * Time.deltaTime), Space.World);
+        transform.Translate(GetDirection(PlayerInput()).normalized * (speed * Time.deltaTime), Space.World);
+    }
+
+    private Vector3 PlayerInput()
+    {
+        var direction = new Vector3(_movement.x, 0, _movement.y);
+        return direction;
     }
 
     private Vector3 GetDirection(Vector3 direction)
@@ -62,15 +78,15 @@ public class PlayerMovement : MonoBehaviour
             //calculates the angle of the direction the player is moving.
             if (_camera != null)
             {
-                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg +
-                                    _camera.transform.eulerAngles.y;
+                var targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg +
+                                  _camera.transform.eulerAngles.y;
                 //it smooths the transition between the transform.eulerAngles.y and the targetAngle.
-                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity,
+                var angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity,
                     turnSmoothTime);
                 //sets the rotation of the player to the angle.
                 transform.rotation = Quaternion.Euler(0f, angle, 0f);
                 //rotates the player to the direction they are moving.
-                Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                var moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
                 return moveDir;
             }
         }
@@ -78,25 +94,10 @@ public class PlayerMovement : MonoBehaviour
         return new Vector3();
     }
 
-    private void CheckSprint()
-    {
-        speed = Input.GetKey(KeyCode.LeftShift) ? 12f : 6f;
-    }
-
-    private void CheckDash()
-    {
-        if (Input.GetKeyDown("q") && _canDash)
-        {
-            StartCoroutine(Dash());
-        }
-        
-    }
-
     private IEnumerator Dash()
     {
         //var direction = lastDirection;
         _canDash = false;
-        Debug.Log("dash false");
         _isDashing = true;
         _rigidbody.useGravity = false;
         //float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg+ Camera.main.transform.eulerAngles.y;
@@ -106,10 +107,9 @@ public class PlayerMovement : MonoBehaviour
         yield return new WaitForSeconds(dashingTime);
         tr.emitting = false;
         _rigidbody.useGravity = true;
-        _rigidbody.velocity = new Vector3(0f,0f,0f);
+        _rigidbody.velocity = new Vector3(0f, 0f, 0f);
         _isDashing = false;
         yield return new WaitForSeconds(dashingCooldown);
-        Debug.Log("dash true");
         _canDash = true;
     }
 }
