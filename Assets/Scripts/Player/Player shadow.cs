8be98643493cdec2cd035a8633142329e2e269ce
tusmavan <17132304@student.hhs.nl>
2023-11-23 13:25:06 +0100

    shadow changes orientation depending on ground orientation

--- Assets/Scripts/Player/Player shadow.cs
+++ Assets/Scripts/Player/Player shadow.cs
@@ -11,26 +11,17 @@ public class Playershadow : MonoBehaviour
     public (float,Vector3) CheckGround()
     {
         var layermask = 1 << 6;
-        Physics.Raycast(transform.position, Vector3.down,out var hit, 30f, ~layermask);
-        var surface = hit.normal;
-        var x = Mathf.Asin(hit.normal.x);
-        var y = Mathf.Asin(hit.normal.y);
-        var z = Mathf.Asin(hit.normal.z);
- 
-        //Debug.Log("Radians:" + x);
- 
-        //Debug.Log("Degrees:" + (x * Mathf.Rad2Deg)+ " y" + (y * Mathf.Rad2Deg)+ "z" + (z * Mathf.Rad2Deg));
-        if(hit.point.y != null) return (hit.point.y,hit.normal);
+        Physics.Raycast(transform.position, Vector3.down,out var hit, 100f, ~layermask);
         
-        return (0f, new Vector3(0,0,0));
+        if(hit.point.y != null) return (hit.point.y,hit.normal);
+
+        return (0f, Vector3.back);
 
     }
 
     public void Update()
     {
-        var normal = CheckGround().Item2;
-        var angle = Vector3.Angle(shadow.transform.forward, normal);
-        var rot = new Vector3(normal.x * Mathf.Rad2Deg,normal.y * Mathf.Rad2Deg,normal.z * Mathf.Rad2Deg);
+        shadow.transform.localRotation = Quaternion.FromToRotation(Vector3.back, CheckGround().Item2);
         shadow.transform.position = new Vector3(shadow.position.x, CheckGround().Item1+.1f, shadow.position.z);
     }
 }
