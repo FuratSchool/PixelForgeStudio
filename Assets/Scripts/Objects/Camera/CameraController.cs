1ae1a68cb16a34b3996f6b589e2f7eef29865702
Mike <mike02.oudshoorn@gmail.com>
2023-12-01 12:52:38 +0100

    corrected speeds with controller

--- Assets/Scripts/Objects/Camera/CameraController.cs
+++ Assets/Scripts/Objects/Camera/CameraController.cs
@@ -3,12 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using Cinemachine;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
     private CinemachineFreeLook _camera;
     [SerializeField] private float _defaultSensitivityX;
     [SerializeField] private float _defaultSensitivityY;
+    [SerializeField] private float ControllerCorrectionX =5;
+    [SerializeField] private float ControllerCorrectionY =5;
+    private float correctionX = 1;
+    private float correctionY = 1;
     private void Awake()
     {
         _camera = GetComponent<CinemachineFreeLook>();
@@ -19,9 +24,24 @@ public class CameraController : MonoBehaviour
 
     public void UpdateCameraSettings(SettingsData settings)
     {
-        _camera.m_YAxis.m_MaxSpeed = _defaultSensitivityY * (settings.sensitivityY);
-        _camera.m_XAxis.m_MaxSpeed = _defaultSensitivityX * (settings.sensitivityX);
+        _camera.m_YAxis.m_MaxSpeed = _defaultSensitivityY * (settings.sensitivityY * correctionY);
+        _camera.m_XAxis.m_MaxSpeed = _defaultSensitivityX * (settings.sensitivityX * correctionX);
         _camera.m_XAxis.m_InvertInput = settings.invertedX;
         _camera.m_YAxis.m_InvertInput = settings.invertedY;
+    }
+    
+    void ControlChanged(PlayerInput input)
+    {
+        var device = input.currentControlScheme;
+        if(device.Equals("Controller"))
+        {
+            correctionX = ControllerCorrectionX;
+            correctionY = ControllerCorrectionY;
+        }
+        else if (device.Equals("KeyboardMouse"))
+        {
+            correctionX = 1;
+            correctionY = 1;
+        }
     }
 }
