98c7299ee1c13e347e7f3bf82144cd5ab64ff60a
Mike <mike02.oudshoorn@gmail.com>
2024-01-09 18:22:14 +0100

    code cleanup

--- Assets/Scripts/Player/PlayerStates/EmoteState.cs
+++ Assets/Scripts/Player/PlayerStates/EmoteState.cs
@@ -3,16 +3,15 @@ using UnityEngine;
 public class EmoteState : IPlayerState
 {
     public EmoteState (PlayerController pc) : base("EmoteState", pc) {_pc = (PlayerController)this._playerStateMachine;}
-
-    private bool _textActive = false;
-    private GameObject chair;
+    
+    private GameObject _chair;
     public override void EnterState()
     {
         _pc.EnableGrimParticles(false);
         _playerStateMachine.Animator.SetInteger("State", 20);
         _pc.waitOver = false;
-        chair = _pc.InstantiateFunc(_pc.Chair, GameObject.Find("ChairSpawnPoint").transform.position, Quaternion.identity);
-        chair.transform.Rotate((chair.transform.rotation.x-90),_pc.transform.localRotation.eulerAngles.y,0);
+        _chair = _pc.InstantiateFunc(_pc.Chair, GameObject.Find("ChairSpawnPoint").transform.position, Quaternion.identity);
+        _chair.transform.Rotate((_chair.transform.rotation.x-90),_pc.transform.localRotation.eulerAngles.y,0);
         _pc.WaitSecs(5);
         base.EnterState();
     }
@@ -25,11 +24,7 @@ public class EmoteState : IPlayerState
 
     public override void ExitState()
     {
-        _pc.DestroyFunc(chair);
+        _pc.DestroyFunc(_chair);
         _pc.EnableGrimParticles(true);
-    }
-
-    public override void LateUpdateState()
-    {
     }
 }
