a36f696818f70f79c068bbea04acc1ea7dd9e280
samsara <18023827@student.hhs.nl>
2024-01-11 19:58:03 +0100

    Revert "Merge branch 'feature/swinging' into WaterFallLevelShaded"
    This reverts commit 78117b0f7c2cbec7c243be55afc1dfb757013104, reversing
    changes made to 8f7d2bcbec80bcb8b017b3d078262aa7ca7b077f.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -65,8 +65,6 @@ public class PlayerController : PlayerStateMachine
     [SerializeField] private float SwingDelay = 1f;
     [SerializeField] public LineRenderer lr;
     [SerializeField] public GameObject Hand;
-    [SerializeField] public GameObject Scythe;
-    [SerializeField] public GameObject Handle;
     
     public string InteractableText { get; } = " To Interact";
     public string DialogueText { get; } = " To Talk";
@@ -378,8 +376,6 @@ public class PlayerController : PlayerStateMachine
         lr.positionCount = 0;
         Destroy(joint);
         _isSwinging = false;
-        Scythe.transform.localPosition = new Vector3(0.0075f, 0, 0.013f);
-        Scythe.transform.localEulerAngles = new Vector3(0, 205, 0);
         player.GetComponent<Rigidbody>().AddForce(ExitForce * Vector3.up, ForceMode.Impulse);
         StartCoroutine(SwingDelayTimer());
     }
